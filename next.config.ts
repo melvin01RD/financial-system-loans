57ef9845b67a1862b754237f731a676136acd667
melvin01RD <melvin01rd@gmail.com>
2026-01-02 21:05:07 -0400

    Limpieza total de caracteres invalidos y config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  devIndicators: { appIsrStatus: false }
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
+
 export default nextConfig;
