6a8088ec41f8040c77f834cfe6bd43cc56ad8a0e
melvin01RD <melvin01rd@gmail.com>
2026-01-02 21:46:16 -0400

    fix: eliminado page.tsx de la raiz para forzar redireccion via middleware

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // 🚀 Esto mandará a cualquier usuario que entre al link directamente al Login
-  redirect('/login');
-}
